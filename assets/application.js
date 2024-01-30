026cf00baeef8460989a243e95d9552a0e734854
AqdasGhalibOSO <aqdasghaliboso21@gmail.com>
2024-01-30 11:40:27 +0500

    Hello Elizabeth

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,11 @@
-// Put your application javascript here
+jQuery(document).ready(function () {
+  jQuery('.slideshow-container').slick({
+      dots: false,
+      infinite: true, 
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: jQuery('.prev-arrow'),
+      nextArrow: jQuery('.next-arrow'),
+  });
+});
